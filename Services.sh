39179d7c8bb477e82a099b8332889be8fe14dffe
ysn224 <s.bella1209@gmail.com>
2025-01-23 20:37:25 -0600

    Update Services.sh

--- Services.sh
+++ Services.sh
@@ -1,50 +1,58 @@
 # VSFTPD
+
 echo -n "Should VSFTP Be Installed/Reinstalled? [Y/n] "
 read option
 if [[ $option =~ ^[Yy]$ ]]
 then
- 	apt-get -y install vsftpd > /dev/null 2>&1 
- 	# Disable anonymous uploads
- 	sed -i '/^anon_upload_enable/ c\anon_upload_enable no   #' /etc/vsftpd.conf # outdated?
- 	sed -i '/^anonymous_enable/ c\anonymous_enable=NO  #' /etc/vsftpd.conf
-	# FTP user directories use chroot
-	sed -i '/^chroot_local_user/ c\chroot_local_user=YES  #' /etc/vsftpd.conf
-	service vsftpd restart
+  sudo apt-get -y install vsftpd > /dev/null 2>&1 
+  # Disable anonymous uploads
+  sudo sed -i '/^anon_upload_enable/ s/=.*/= NO/' /etc/vsftpd.conf
+  sudo sed -i '/^anonymous_enable/ s/=.*/= NO/' /etc/vsftpd.conf
+  # FTP user directories use chroot
+  sudo sed -i '/^chroot_local_user/ s/=.*/= YES/' /etc/vsftpd.conf
+  sudo systemctl restart vsftpd
 else
-	dpkg --purge vsftpd > /dev/null 2>&1 
+  sudo dpkg --purge vsftpd > /dev/null 2>&1 
 fi
 
 
+
 # Apache2
+#!/bin/bash
+
 echo -n "Should Apache2 Be Installed/Reinstalled? [Y/n] "
 read option
 if [[ $option =~ ^[Yy]$ ]]
 then
- 	apt-get install apache2 libapache2-mod-php5  > /dev/null 2>&1 
-file=$( echo /etc/apache2/conf-enabled/security.conf )
-#replace ServerTokens and ServerSignature
-sed -i 's/ServerTokens/ServerTokens Prod  # /g' $file
-sed -i 's/ServerSignature/ServerSignature Off # /g' $file
-echo "<Directory />
-    		Options -Indexes 
-		</Directory>" >> $file
-#Critical File Permissions
-	chown -R root:root /etc/apache2
-	chown -R root:root /etc/apache
+  sudo apt-get install apache2 libapache2-mod-php7.4 > /dev/null 2>&1
+  file="/etc/apache2/conf-enabled/security.conf"
+  
+  # Replace ServerTokens and ServerSignature
+  sudo sed -i 's/^ServerTokens.*/ServerTokens Prod/' $file
+  sudo sed -i 's/^ServerSignature.*/ServerSignature Off/' $file
+  
+  # Append directory settings
+  echo "<Directory />
+        Options -Indexes
+  </Directory>" | sudo tee -a $file
+  
+  # Critical File Permissions
+  sudo chown -R root:root /etc/apache2
 
-	#Secure Apache 2
-	if [[ -e /etc/apache2/apache2.conf ]]; then
-		echo \<Directory \> >> /etc/apache2/apache2.conf
-		echo -e ' \t AllowOverride None' >> /etc/apache2/apache2.conf
-		echo -e ' \t Order Deny,Allow' >> /etc/apache2/apache2.conf
-		echo -e ' \t Deny from all' >> /etc/apache2/apache2.conf
-		echo \<Directory \/\> >> /etc/apache2/apache2.conf
-		echo UserDir disabled root >> /etc/apache2/apache2.conf
-	fi
-#THIS MAY BREAK APACHE2, NOT ENTIRELY SURE, TEST FIRST
-
- else
-	  dpkg --purge apache2 > /dev/null 2>&1 
+  # Secure Apache2
+  if [[ -e /etc/apache2/apache2.conf ]]; then
+    sudo echo -e "<Directory />" | sudo tee -a /etc/apache2/apache2.conf
+    sudo echo -e "    AllowOverride None" | sudo tee -a /etc/apache2/apache2.conf
+    sudo echo -e "    Order Deny,Allow" | sudo tee -a /etc/apache2/apache2.conf
+    sudo echo -e "    Deny from all" | sudo tee -a /etc/apache2/apache2.conf
+    sudo echo -e "</Directory>" | sudo tee -a /etc/apache2/apache2.conf
+    sudo echo "UserDir disabled root" | sudo tee -a /etc/apache2/apache2.conf
+  fi
+  
+  # Restart Apache
+  sudo systemctl restart apache2
+else
+  sudo dpkg --purge apache2 > /dev/null 2>&1
 fi
 
 
@@ -53,42 +61,60 @@ echo -n "Should MySQL Be Installed/Reinstalled? [Y/n] "
 read option
 if [[ $option =~ ^[Yy]$ ]]
 then
- 	apt-get install mysql-server php5-mysql -y > /dev/null 2>&1 
-	mysql_secure_installation
-file=$( echo /etc/mysql/my.cnf )
-#bind-address = 127.0.0.1 #
-sed -i 's/bind-address/bind-address = 127.0.0.1 # /g' $file
-service mysql restart
+  # Install MySQL and PHP MySQL extension
+  apt-get install mysql-server php5-mysql -y > /dev/null 2>&1 
 
- else
-	  dpkg --purge mysql-server php5-mysql > /dev/null 2>&1 
+  # Run MySQL secure installation (this will prompt you for setting a root password, removing insecure defaults, etc.)
+  mysql_secure_installation
+
+  # Define the path to the MySQL config file
+  file="/etc/mysql/my.cnf"
+
+  # Modify the bind-address to allow MySQL to only listen on localhost (127.0.0.1)
+  sed -i 's/^bind-address.*/bind-address = 127.0.0.1 # /g' $file
+
+  # Restart MySQL service to apply the changes
+  service mysql restart
+
+else
+  # Purge MySQL and its PHP extension if the user chooses to uninstall
+  dpkg --purge mysql-server php5-mysql > /dev/null 2>&1 
 fi
 
 
-# Php
+
+# PHP
 echo -n "Should PHP5 Be Installed/Reinstalled? [Y/n] "
 read option
 if [[ $option =~ ^[Yy]$ ]]
 then
- 	apt-get install python-software-properties -y > /dev/null 2>&1 
-	add-apt-repository ppa:ondrej/php5-oldstable
-	apt-get update -y > /dev/null 2>&1 
-	apt-get install -y php5 > /dev/null 2>&1 
-	file=$(echo /etc/php5/apache2/php.ini)
+  # Install python-software-properties package (needed for adding repositories)
+  apt-get install python-software-properties -y > /dev/null 2>&1 
 
-	#At the end of each of these lines is a ; instead of a #, this is b/c this configuration has different syntax than bash and the ; tells it to comment the rest out.
+  # Add PPA repository for PHP5 (old stable)
+  add-apt-repository ppa:ondrej/php5-oldstable
+  apt-get update -y > /dev/null 2>&1 
 
-	sed -i 's/expose_php/expose_php=Off ; /g' $file
-sed -i 's/allow_url_fopen/allow_url_fopen=Off ; /g' $file
-sed -i 's/allow_url_include/allow_url_include=Off ; /g' $file
-#disable_functions 
-sed -i 's/disable_functions=/disable_functions=exec,shell_exec,passthru,system,popen,curl_exec,curl_multi_exec,parse_ini_file,show_source,proc_open,pcntl_exec,/g' $file
-sed -i 's/upload_max_filesize/upload_max_filesize = 2M ; /g' $file
-sed -i 's/max_execution_time/max_execution_time = 30 ; /g' $file
-sed -i 's/max_input_time/max_input_time = 60 ; /g' $file
+  # Install PHP5
+  apt-get install -y php5 > /dev/null 2>&1 
+
+  # Define the path to the PHP configuration file
+  file="/etc/php5/apache2/php.ini"
+
+  # Modify PHP settings using sed
+  sed -i 's/expose_php/expose_php=Off ; /g' $file
+  sed -i 's/allow_url_fopen/allow_url_fopen=Off ; /g' $file
+  sed -i 's/allow_url_include/allow_url_include=Off ; /g' $file
+  sed -i 's/disable_functions=/disable_functions=exec,shell_exec,passthru,system,popen,curl_exec,curl_multi_exec,parse_ini_file,show_source,proc_open,pcntl_exec,/g' $file
+  sed -i 's/upload_max_filesize/upload_max_filesize = 2M ; /g' $file
+  sed -i 's/max_execution_time/max_execution_time = 30 ; /g' $file
+  sed -i 's/max_input_time/max_input_time = 60 ; /g' $file
+
 else
-	  dpkg --purge php5 > /dev/null 2>&1 
+  # If user chooses "no", purge PHP5
+  dpkg --purge php5 > /dev/null 2>&1 
 fi
+
 
 
 # SSH
@@ -96,109 +122,77 @@ echo -n "Should SSH Be Installed/Reinstalled? [Y/n] "
 read option
 if [[ $option =~ ^[Yy]$ ]]
 then
-apt-get install ssh openssh-server openssh-client -y > /dev/null 2>&1 
-#goes and replaces the /etc/ssh/sshd_config with clean one
-echo "Replacing /etc/ssh/sshd_config" >> WorkProperly.txt
-cp /etc/ssh/sshd_config /etc/ssh/.sshd_config
-echo "# Package generated configuration file
+  # Install SSH components
+  apt-get install ssh openssh-server openssh-client -y > /dev/null 2>&1 
+
+  # Log the action of replacing sshd_config
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - Replacing /etc/ssh/sshd_config" >> WorkProperly.txt
+
+  # Backup the current sshd_config before overwriting
+  cp /etc/ssh/sshd_config /etc/ssh/sshd_config.backup
+  if [[ $? -eq 0 ]]; then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - Backup of sshd_config successful." >> WorkProperly.txt
+  else
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - Backup of sshd_config failed." >> WorkProperly.txt
+  fi
+
+  # Replace the sshd_config with the new configuration
+  echo "# Package generated configuration file
 # See the sshd_config(5) manpage for details
 # What ports, IPs and protocols we listen for
 Port 22
-# Use these options to restrict which interfaces/protocols sshd will bind to
-#ListenAddress ::
-#ListenAddress 0.0.0.0
 Protocol 2
-# HostKeys for protocol version 2
 HostKey /etc/ssh/ssh_host_rsa_key
 HostKey /etc/ssh/ssh_host_dsa_key
 HostKey /etc/ssh/ssh_host_ecdsa_key
-#Privilege Separation is turned on for security
 UsePrivilegeSeparation yes
-# Lifetime and size of ephemeral version 1 server key
 KeyRegenerationInterval 3600
 ServerKeyBits 768
-# Logging
 SyslogFacility AUTH
 LogLevel INFO
-
-# Authentication:
 LoginGraceTime 120
 PermitRootLogin no
 StrictModes yes
-
 RSAAuthentication yes
 PubkeyAuthentication yes
-#AuthorizedKeysFile	%h/.ssh/authorized_keys
-
-# Don't read the user's ~/.rhosts and ~/.shosts files
 IgnoreRhosts yes
-# For this to work you will also need host keys in /etc/ssh_known_hosts
 RhostsRSAAuthentication no
-# similar for protocol version 2
 HostbasedAuthentication no
-# Uncomment if you don't trust ~/.ssh/known_hosts for RhostsRSAAuthentication
-#IgnoreUserKnownHosts yes
-# To enable empty passwords, change to yes (NOT RECOMMENDED)
-
 PermitEmptyPasswords no
-# Change to yes to enable challenge-response passwords (beware issues with
-# some PAM modules and threads)
 ChallengeResponseAuthentication no
-
-# Change to no to disable tunnelled clear text passwords
-#PasswordAuthentication yes
-#PublicKeyAuthentication yes
-
-# Kerberos options
 KerberosAuthentication no
-#KerberosGetAFSToken no
-#KerberosOrLocalPasswd yes
-#KerberosTicketCleanup yes
-
-# GSSAPI options
 GSSAPIAuthentication no
-#GSSAPICleanupCredentials yes
 X11Forwarding no
-
 X11DisplayOffset 10
 PrintMotd no
 PrintLastLog yes
 TCPKeepAlive yes
-#UseLogin no
-
-#client alive
 ClientAliveInterval 60
 ClientAliveCountMax 3
-
-#extra
 MaxAuthTries 3
 LoginGraceTime 20
 PermitUserEnvironment no
 AllowAgentForwarding no
 AllowTcpForwarding no
 PermitTunnel no
-
-#MaxStartups 10:30:60
-#Banner /etc/issue.net
-# Allow client to pass locale environment variables
 AcceptEnv LANG LC_*
-
 Subsystem sftp /usr/lib/openssh/sftp-server
-
-# Set this to 'yes' to enable PAM authentication, account processing,
-# and session processing. If this is enabled, PAM authentication will
-# be allowed through the ChallengeResponseAuthentication and
-# PasswordAuthentication.  Depending on your PAM configuration,
-# PAM authentication via ChallengeResponseAuthentication may bypass
-# the setting of \"PermitRootLogin without-password\".
-# If you just want the PAM account and session checks to run without
-# PAM authentication, then enable this but set PasswordAuthentication
-# and ChallengeResponseAuthentication to 'no'.
 UsePAM yes" > /etc/ssh/sshd_config
-service ssh restart
-echo "" >> WorkProperly.txt
-echo "Finished with SSH"
+
+  # Restart SSH service to apply changes
+  service ssh restart
+  if [[ $? -eq 0 ]]; then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - SSH service restarted successfully." >> WorkProperly.txt
+  else
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - SSH service restart failed." >> WorkProperly.txt
+  fi
+
+  # Log completion
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - Finished with SSH configuration" >> WorkProperly.txt
 
 else
-	dpkg --purge ssh openssh-server openssh-client > /dev/null 2>&1 
+  # Purge SSH if the user opts out
+  dpkg --purge ssh openssh-server openssh-client > /dev/null 2>&1 
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - SSH uninstalled." >> WorkProperly.txt
 fi
+
